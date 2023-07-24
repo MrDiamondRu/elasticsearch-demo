28317a6f86484482e638e5e5930f1bef25abce8f
Ruslan <r.nikonov-pro@yandex.ru>
2023-07-24 11:40:24 +0300

    Initial commit

---
+++ src/main/java/ru/work/group7/elasticsearchdemo/ElasticsearchDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.work.group7.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemoApplication.class, args);
+    }
+
+}
